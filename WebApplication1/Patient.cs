e81d5259f3c544ce34853ddbabfc6d5a65a2e623
Dulina Gunasekara <83606238+Dulinag@users.noreply.github.com>
2023-04-22 20:15:32 -0400

    Create Patient.cs

---
+++ WebApplication1/Patient.cs
@@ -0,0 +1,7 @@
+public class Patient
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Address { get; set; }
+    public string PhoneNumber { get; set; }
+}
